a0c873ffe1655372064154249c0e4dc57d28ab4e
ikromjonxursanov <xursanovikromjon2@gmail.com>
2026-05-05 11:35:55 +0500

    refactor(settings):

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.render.com',]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # STATICFILES_DIRS=[
 #     BASE_DIR / 'static'
